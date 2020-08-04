81e8cbf5dfe7fd0d1520e263e34bb9aa3c9394eb
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2020-08-04 22:14:28 +0200

    remove old glObjects

--- simpleOpenGL/src/gl/loadGL.h
+++
@@ -1,6 +0,0 @@
-#include "./objects/VertexArray.h"
-#include "./objects/VertexBufferObject.h"
-#include "./objects/ElementArrayBuffer.h"
-#include "./objects/Texture.h"
-#include "./objects/TextureAtlas.h"
-#include "./shaders/Shader.h"
